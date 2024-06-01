44285c2cd4e65e0fc3444bf0318a50d343c28c8c
LuisChanocua <esteban083057@gmail.com>
2024-06-01 16:30:10 -0600

    new branch services registro qr

---
+++ lib/services/registerqrs_service.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegistroQRService extends StatefulWidget {
+  const RegistroQRService({super.key});
+
+  @override
+  State<RegistroQRService> createState() => _RegistroQRServiceState();
+}
+
+class _RegistroQRServiceState extends State<RegistroQRService> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
